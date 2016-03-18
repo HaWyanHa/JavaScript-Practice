c68589cd059ecdde3f0d8cc88a9afd2cfb9cd2f2
footclan2k2 <HaWyanHa@Ryans-MacBook-Pro-2.local>
2016-03-18 16:03:29 -0400

    got the rotate to work

--- game.js
+++ game.js
@@ -32,7 +32,7 @@
     function handleKeys(event) {
         console.log(event.keyCode);
         if (event.keyCode === 38) {
-            if (ship.velocity === 10) {
+            if (ship.velocity >= 10) {
                 ship.velocity = 10;
             } else {
             ship.velocity = ship.velocity + 1;
@@ -40,7 +40,7 @@
                     }
         }
         else if (event.keyCode === 40) {
-            if (ship.velocity === 0) {
+            if (ship.velocity <= 0) {
                 ship.velocity = 0;
             } else {
             ship.velocity = ship.velocity - 1;
@@ -49,10 +49,13 @@
         }
         else if (event.keyCode === 37) {
             ship.angle = ship.angle - 15;
+            ship.element.style.transform = "rotate(" + ship.angle + "deg)";
+            console.log(ship.element.style.transform);
             console.log(ship.angle);
             }
         else if (event.keyCode === 39) {
             ship.angle = ship.angle +15;
+            ship.element.style.transform = "rotate(" + ship.angle + "deg)";
             console.log(ship.angle);
         }
 
